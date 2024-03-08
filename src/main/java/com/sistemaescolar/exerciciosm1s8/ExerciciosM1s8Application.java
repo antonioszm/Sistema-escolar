baca4c7adc4101aee9165abf6e70fbc4036b1d5c
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-03-08 16:56:53 -0300

    commit inical

---
+++ src/main/java/com/sistemaescolar/exerciciosm1s8/ExerciciosM1s8Application.java
@@ -0,0 +1,13 @@
+package com.sistemaescolar.exerciciosm1s8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosM1s8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosM1s8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaescolar/exerciciosm1s8/ExerciciosM1s8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaescolar.exerciciosm1s8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosM1s8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
